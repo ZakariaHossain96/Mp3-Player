6be6a0f5a390b661003202733a075bb512da746b
Zakaria Hossain <zakariahossain20155@gmail.com>
2020-02-02 12:31:48 +0600

    Initial Commit

---
+++ Mp3 Player/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Mp3 Player
+//
+//  Created by Mac on 2/2/20.
+//  Copyright © 2020 Zakaria. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
